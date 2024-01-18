ce5a81d0c77b0f4b0320863362873ad9e65a45ab
2OOTHZERO <151498349+lgy1543@users.noreply.github.com>
2024-01-18 16:36:37 +0900

    01-18 메인베너(section1-html 완료)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
 // 가영님 자바스크립트
+const swiper = new Swiper('.slide-intro', {
+
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
