d8a499d6d1479847ec9f6f8e48ddf9d83ee02e92
hyeeons <qouop0101@gmail.com>
2024-01-23 18:20:10 +0900

    240123_건강정보_건강TV 스와이퍼_이현정

--- assets/js/healthCare.js
+++ assets/js/healthCare.js
@@ -3,4 +3,19 @@ var swiper = new Swiper(".swiper-nutritions", {
   slidesPerView: 3,
   spaceBetween: 30,
   loop: true,
+  autoplay: {
+    delay: 2300,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper(".swiper-healthtv", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
