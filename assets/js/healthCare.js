96541eb1271325e33f81612a9b8ff57193bc57a7
hyeeons <qouop0101@gmail.com>
2024-01-26 18:08:17 +0900

    240126_건강정보_건강코치 슬라이드구현_이현정

--- assets/js/healthCare.js
+++ assets/js/healthCare.js
@@ -19,20 +19,6 @@ var swiper = new Swiper(".swiper-healthtv", {
   },
 });
 
-var swiper = new Swiper(".swiper-coach", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: ".coach-next",
-    prevEl: ".coach-prev",
-  },
-});
-
 document.addEventListener("DOMContentLoaded", function () {
   const tabs = document.querySelectorAll(".sec-tabs");
 
@@ -53,5 +39,18 @@ document.addEventListener("DOMContentLoaded", function () {
     tabBtn.addEventListener("click", function () {
       tabBtn.classList.toggle("active");
     });
+  });
+});
+
+$(document).ready(function () {
+  $(".slide-coach-wrapper").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button class="slick-prev" aria-label=""></button>',
+    nextArrow: '<button class="slick-next" aria-label=""></button>',
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 2900,
   });
 });
